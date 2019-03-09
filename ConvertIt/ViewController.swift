c19fc5e829662b8a5b0974323f54e58e37c3fb72
Henry Chen <46900345+henry256@users.noreply.github.com>
2019-03-09 12:45:27 -0500

    Add files via upload

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultsLabel: UILabel!
     @IBOutlet weak var formulaPicker: UIPickerView!
     
+    
     var formulaArray=["Miles to Kilometers",
                       "Kilometers to Miles",
                       "Feet to Meters",
@@ -27,14 +28,14 @@ class ViewController: UIViewController {
     var conversionString=""
     override func viewDidLoad() {
         super.viewDidLoad()
-        formulaPicker.delegate=self
-        formulaPicker.dataSource=self
+        formulaPicker.delegate = self
+        formulaPicker.dataSource = self
 
     }
     func calcuLateConvert(){
         
         var outputValue=0.0
-        if let inputValue=Double(userInput.text!){
+        if let inputValue=Double(userInput.text!){	
             switch conversionString{
             case "Miles to Kilometers":
             outputValue=inputValue/0.62137
@@ -49,9 +50,10 @@ class ViewController: UIViewController {
             case "Meters to Yards":
             outputValue=inputValue/0.62137
             default:
-            outputValue=0
+            print("no convertable string")
             
         }
+            resultsLabel.text="\(inputValue)\(fromUnits)=\(outputValue)\(toUnits)"
         }
         else{
             print("value is not a number")
@@ -59,6 +61,15 @@ class ViewController: UIViewController {
         
     }
     
+   
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+    }
+}
+extension ViewController:UIPickerViewDelegate ,UIPickerViewDataSource{
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return formulaArray.count
     }
@@ -70,9 +81,8 @@ class ViewController: UIViewController {
         conversionString=formulaArray[row]
         fromUnits=unitsArray[0]
         toUnits=unitsArray[1]
-        
+    
         FromUnitsLabel.text=fromUnits
         calcuLateConvert()
-    }
 }
-
+}
